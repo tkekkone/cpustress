292bc09d73287a7273318a998fe2e05b596fef9d
Tuomas Kekkonen <tkekkone@gmail.com>
2019-02-13 17:25:53 +0200

    Add script. Takes one parameter which defines the event count which need to get reached to keep stressing. thread count is read from nproc

---
+++ keeprunning.sh
@@ -0,0 +1,14 @@
+result=$1
+cores=$(nproc)
+while [ $result -ge $1 ]; do
+	if [ $result -ne $1 ]
+	then
+		sleep 20s;
+	fi
+	runresult=$(sysbench --threads=$cores --time=30 cpu run)
+	echo "$runresult"
+	result=$(echo "$runresult" | grep -i "total number of events"| awk '{print $5}')
+	echo "Result was $result."
+done
+
+
